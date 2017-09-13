bba5f5889b5b949fd3e3141bbb804acac2a61188
squidcarroll <squidcarroll@gmail.com>
2017-09-13 22:01:02 +0000

    Formatting for c files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+*main - Prints statemnet and returns 0
+*Description:Prints out a section of a quote and returns 0
+* Return: prints and returns 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
