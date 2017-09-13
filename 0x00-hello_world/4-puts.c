01d51621aea7630a7288d37f17b5b6bf40080b3b
squidcarroll <squidcarroll@gmail.com>
2017-09-13 21:55:10 +0000

    add desctption for puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/*Displays the quote in the command line*/
-
-int main()
+/**
+*main - Displays the quote in the command line
+*
+*Description - Prints out the quote and returns 0
+*Return: The quote
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilangual puzzle");
 
