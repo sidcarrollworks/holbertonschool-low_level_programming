d6d1a26e2f3e34e0c4a3ec46ab77a41eac71b4e9
squidcarroll <squidcarroll@gmail.com>
2017-09-13 22:15:52 +0000

    formatting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
 * main - Displays the quote in the command line
-* Description - Prints out the quote and returns 0
+* Description: Prints out the quote and returns 0
 * Return: The quote
 */
 int main(void)
