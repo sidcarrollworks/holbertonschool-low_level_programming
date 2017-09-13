a3486839248beeb61aa18372c0feff704a2c35ab
squidcarroll <squidcarroll@gmail.com>
2017-09-13 22:18:32 +0000

    adsf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - Displays the quote in the command line
-* Description: Prints out the quote and returns 0
+*main - Displays the quote in the command line
+*
 * Return: The quote
 */
 int main(void)
