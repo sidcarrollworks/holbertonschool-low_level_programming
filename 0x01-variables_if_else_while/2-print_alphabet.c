f071792ab0b904755f90f26577bcc242513d778f
squidcarroll <squidcarroll@gmail.com>
2017-09-14 19:29:15 +0000

    number 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet
+ * Return: the alphabet is printed out
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar (c);
+		putchar ('\n');
+
+	return (0);
+}
