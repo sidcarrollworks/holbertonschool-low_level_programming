60ca4f4ced104cafb68c41b51e64e9e20b4185e2
squidcarroll <squidcarroll@gmail.com>
2017-09-19 20:20:11 +0000

    description fixes for number 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 
 /**
+ *print_alphabet - Prints out the alphabet
  *
- *
- *
+ *Description: Prints out the alphabet using a loop withascii code
+ *Result: the alphabet
  */
 void print_alphabet(void)
 {
 	int c;
-	
+
 	for (c = 'a' ; c < 'z' ; c++)
 	{
 		_putchar(c);
