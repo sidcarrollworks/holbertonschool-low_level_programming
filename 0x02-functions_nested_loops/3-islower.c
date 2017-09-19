4d446feb32f24637ffd7e03f6b1b4adbca7a28a2
squidcarroll <squidcarroll@gmail.com>
2017-09-19 21:17:37 +0000

    fix description and spacing for 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,15 @@
 /**
  * _islower - checks for lowercase letter
  *
- * Description: 
+ * Description: checks if letter is between the ascii values for lowercase
+ * @c: is the input from the test file
  * Return: 1 if lowercase 0 if otherwise
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
 
