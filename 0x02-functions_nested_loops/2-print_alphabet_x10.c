5e549cb933a2aa7908f49ddd2220b252562eeb6d
squidcarroll <squidcarroll@gmail.com>
2017-09-19 20:39:04 +0000

    add task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - prints out the alphabet 10 times each on a different line
+ *
+ *Description: uses nested for loop to accomplish this
+ *Result: 10 prints alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int c, d;
+
+	for (d = 0 ; d <= 9 ; d++)
+	{
+		for (c = 'a' ; c < 'z' ; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
