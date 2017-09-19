3e40c0374f83464d0e42deb5ac2cb59639b9d507
squidcarroll <squidcarroll@gmail.com>
2017-09-19 20:06:44 +0000

    activities 0 and 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	int c;
+	
+	for (c = 'a' ; c < 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif
