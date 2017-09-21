0582078d37d5c0902a346ac441e9e7b4b7cb6b74
squidcarroll <squidcarroll@gmail.com>
2017-09-21 20:41:02 +0000

    fix for number 6

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -11,10 +11,12 @@ void print_line(int n)
 {
 	int a;
 
-	for (a = 0 ; a <= n ; a++)
+	if (n > 0)
 	{
-		if (n > 0)
+		for (a = 0 ; a < n ; a++)
 			_putchar('_');
+		_putchar('\n');
 	}
+	else
 		_putchar('\n');
 }
