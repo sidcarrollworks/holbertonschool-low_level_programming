cf431a17eef03d850c4b96aef6fac7d5f7eb05a4
squidcarroll <squidcarroll@gmail.com>
2017-09-22 03:03:40 +0000

    add number 11

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
 /**
+ * main - Prints out the largest prime factor
  *
- *
- *
+ * Description: finds all the prime factors and prints the largest one
+ * Return: the largest prime factor
  */
 int main(void)
 {
-	long num;
-int div;
+	long int num = 612852475143;
+	int i;
 
-	num = 612852475143
-
-	for (int i = 2 ; i <= num ; i++)
+	for (i = 2; i <= num ; i++)
 	{
 		if (num % i == 0)
 		{
-			for (int j = 2 ; j < i ; j++)
-			{
-				if (i % j == 0 && j != i)
-					break;
-				else
-					div = i;
-			}
+			num /= i;
+			i--;
 		}
 	}
-	printf("%i\n", div);
+	printf("%d\n", i);
+	return (i);
 }
+
