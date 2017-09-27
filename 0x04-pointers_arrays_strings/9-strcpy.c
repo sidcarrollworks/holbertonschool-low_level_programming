0d0b0e124151dfa82f980718b21793a6d70d3df1
squidcarroll <squidcarroll@gmail.com>
2017-09-27 16:24:10 +0000

    9 test

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] != '\0')
+	while (src[i] >= '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
-	dest[i] = src[i];
+	/*dest[i] = src[i];*/
 	return (dest);
 }
