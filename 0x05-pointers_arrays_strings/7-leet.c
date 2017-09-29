3f10a425ead047ded5a509f4e67341ac9461f1b1
squidcarroll <squidcarroll@gmail.com>
2017-09-29 06:17:30 +0000

    fix syntax

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
+ * leet - encodes string with 1337
  *
- *
- *
- *
+ * Description: econdes a string by replaces letters
+ * @arr: pointer to string
+ * Return: encoded string
  */
 char *leet(char *arr)
 {
@@ -21,11 +22,9 @@ char *leet(char *arr)
 		{
 			if (l[s] == arr[i])
 				arr[i] = n[s];
-		s++;
+			s++;
 		}
 		i++;
 	}
 	return (arr);
 }
-
-	
