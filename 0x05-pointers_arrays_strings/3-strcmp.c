f4852d0771c942845cad9d1b2388488cbe27b74a
squidcarroll <squidcarroll@gmail.com>
2017-09-29 01:12:25 +0000

    add number 3

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two strings and returns the difference
+ *
+ * Description: compares the ascii value of each char in string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns the diffrence of the ascii values
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0')
+	{
+		if (*s2 == '\0')
+			return (0);
+		else if (*s2 > *s1)
+			return (s1 - s2 + 1);
+		else if (*s1 > *s2)
+			return (s1 - s2 - 1);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
