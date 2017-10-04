7fa0e890c8e4fd24330aec2f53fd2e8b4c74ea89
squidcarroll <squidcarroll@gmail.com>
2017-10-04 23:42:10 +0000

    betty for number 5

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -2,20 +2,27 @@
 
 int helper(int x, int n);
 /**
+ * _sqrt_recursion - prints a square root
  *
- *
- *
+ * Description: finds the square root of a number
+ * @n: number to get square root of
  */
 int _sqrt_recursion(int n)
 {
-	return helper(1, n);
+	return (helper(1, n));
 }
 
+/**
+ * helper - does the actual calculations
+ * @x: cycles through the numbers
+ * @n: the number we want to square root
+ * Return: the square root
+ */
 int helper(int x, int n)
 {
 	if (x * x == n)
 		return (x);
 	else if (x * x > n)
 		return (-1);
-	return helper(x + 1, n);
+	return (helper(x + 1, n));
 }
