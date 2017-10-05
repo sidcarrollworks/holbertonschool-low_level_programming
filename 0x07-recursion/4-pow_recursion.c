4a39a9e23b7a71f94b4c2ef852f76568492b88f2
squidcarroll <squidcarroll@gmail.com>
2017-10-05 00:32:17 +0000

    number 4 betty

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 
 /**
+ * _pow_recursion - returns the powers
  *
- *
- *
+ * @x: the base number
+ * @y: the exponent
+ * Return: x raised to y
  */
 int _pow_recursion(int x, int y)
 {
@@ -11,5 +13,5 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
