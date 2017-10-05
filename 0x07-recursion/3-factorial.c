a8b036747c4342c0225435ccbe89c2a594fc4359
squidcarroll <squidcarroll@gmail.com>
2017-10-05 00:37:31 +0000

    betty for 3

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * factorial - find the factorial
+ * @n: the number we want to find the factorial
+ * Return: return the answer
  */
 int factorial(int n)
 {
@@ -11,5 +11,5 @@ int factorial(int n)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	return n * factorial(n - 1);
+	return (n * factorial(n - 1));
 }
