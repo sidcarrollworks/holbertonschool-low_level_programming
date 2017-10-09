07ef483149f2fcad06eefe3c622b662cb0f9f910
squidcarroll <squidcarroll@gmail.com>
2017-10-09 01:27:30 +0000

    betty

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
+ * main - prints the program name
  *
- *
+ * @argc: not used
+ * @argv: the arguement array
+ * Return: returns 0
  */
 int main(int argc __attribute__((unused)), char* argv[])
 {
