d6b021164b33bf375bcf81503968d7f0a3075202
squidcarroll <squidcarroll@gmail.com>
2017-10-09 01:35:05 +0000

    betty for 1

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -3,11 +3,11 @@
 /**
  * main - prints the number of arguements passed into it
  *
- * argc: arguement count
- * argv: not used
+ * @argc: arguement count
+ * @argv: not used
  * Return: 0
  */
-int main(int argc, char* argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	argc -= 1;
 	printf("%d\n", argc);
