1c73580dc0530cf1c0924e9b79dacce858e682a4
squidcarroll <squidcarroll@gmail.com>
2017-10-09 00:51:33 +0000

    remake file

---
+++ 0x08-static_libraries/cfiles/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locates a substring in a string
+ *
+ * Description: finds the matching bytes of substring in string
+ * @haystack: pointer to the string to search in
+ * @needle: pointer to the substring of searched for bytes
+ * Return: the matched bytes otherwise null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *Begin = haystack;
+		char *pattern = needle;
+
+		while (*haystack && *pattern && *haystack == *pattern)
+		{
+			haystack++;
+			pattern++;
+		}
+		if (!*pattern)
+			return (Begin);
+
+		haystack = Begin + 1;
+	}
+	return (0);
+}
