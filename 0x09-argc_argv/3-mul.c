444de0d4a935074c2d801e5f813654c8982f822b
squidcarroll <squidcarroll@gmail.com>
2017-10-09 04:04:53 +0000

    fix fo 3

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -12,15 +12,15 @@ int main(int argc, char *argv[])
 {
 	int sum = 0;
 
-	if (argc >= 2)
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
 		return (0);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 }
