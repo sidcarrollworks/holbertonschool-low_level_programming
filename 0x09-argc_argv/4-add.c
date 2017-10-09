7a578ea95a1602b2138886aba454da8467cec485
squidcarroll <squidcarroll@gmail.com>
2017-10-09 04:42:08 +0000

    fix for 4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -13,25 +13,23 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	argc += 1;
-	if (argc == 0)
+	if (argc < 2)
 	{
-		for (i = 0 ; i < argc ; i++)
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1 ; i < argc ; i++)
+	{
+		for (j = 0 ; argv[i][j] != '\0' ; j++)
 		{
-			if (isdigit(argv[i]))
-			{
-				for (j = 0 ; j < argc ; j++)
-				{
-					sum += atoi(argv[i]);
-				}
-			}
-			else
+			if (isalpha(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
+		sum += atoi(argv[i]);
 	}
-	printf("\n");
+	printf("%d\n", sum);
 	return (0);
 }
