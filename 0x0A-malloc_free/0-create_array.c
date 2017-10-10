6589f347d2bc493163e762fc6b3cd4ba969dedda
squidcarroll <squidcarroll@gmail.com>
2017-10-10 20:38:23 +0000

    fix for 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,13 +1,19 @@
 #include <stdlib.h>
 #include "holberton.h"
 
+/**
+ * create_array - creates an array of chars
+ * @size: tells function what the size of array should be
+ * @c: tells function what to put in array
+ * Return: returns null if size is 0
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *s;
-	int i;
+	unsigned int i;
 
 	s = malloc((size + 1) * sizeof(char));
-	if (s == NULL)
+	if (size == 0)
 	{
 		return (NULL);
 	}
