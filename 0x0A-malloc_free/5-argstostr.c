a5fee4ebd0c04f369dfeb110379c8a2117507cf8
squidcarroll <squidcarroll@gmail.com>
2017-10-11 05:43:28 +0000

    betty for 5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -16,10 +16,10 @@ char *argstostr(int ac, char **av)
 	char *s;
 
 	if (ac == 0 || av == NULL)
-		return(NULL);
+		return (NULL);
 	len = findlength(ac, av);
 	s = malloc(len + 1);
-	for (i = 0 , x = 0 ; i < ac ; i++, x++)
+	for (i = 0, x = 0 ; i < ac ; i++, x++)
 	{
 		for (j = 0 ; av[i][j] != '\0' ; j++, x++)
 			s[x] = av[i][j];
@@ -30,14 +30,14 @@ char *argstostr(int ac, char **av)
 }
 
 /**
- * findlength - finds the space needed 
+ * findlength - finds the space needed
  * @ac: number of arguements
  * @av: the strings
  * Return: the length in bytes needed for malloc
  */
 int findlength(int ac, char **av)
 {
-	int a, b ,c;
+	int a, b, c;
 
 	for (a = 0 ; a < ac ; a++)
 	{
