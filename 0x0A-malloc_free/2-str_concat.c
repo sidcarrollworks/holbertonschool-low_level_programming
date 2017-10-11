7b5914cb5dd0f041f70d338dfac0fca411d358d7
squidcarroll <squidcarroll@gmail.com>
2017-10-11 00:15:38 +0000

    fix for 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -14,7 +14,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j;
 	unsigned int len1, len2, total;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
+		s2 = "";
+	else if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
