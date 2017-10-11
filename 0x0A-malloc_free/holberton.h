d81af66de5a88eb2cd795aac58991224fe0326a6
squidcarroll <squidcarroll@gmail.com>
2017-10-11 00:07:03 +0000

    add number 2

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -2,7 +2,9 @@
 #define HOLBERTON
 
 int _putchar(char c);
+int _strlen(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif
