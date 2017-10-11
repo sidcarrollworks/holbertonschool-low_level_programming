f76c6eb0a64882b18f8b257df73c0cc14a28bf46
squidcarroll <squidcarroll@gmail.com>
2017-10-11 02:13:30 +0000

    number 3

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr = (int **)malloc(height * sizeof(int *));
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height ; i++)
+	{
+		arr[i] = (int *)malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			for (i -= 1 ; i > height ; i--)
+				free(arr[i]);
+			free(arr);
+		}
+	}
+
+	for (i = 0 ; i < height ; i++)
+	{
+		for (j = 0 ; j < width ; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
