1afd553eea48356c05d21eda7aeca6e6561acff9
squidcarroll <squidcarroll@gmail.com>
2017-10-16 21:47:53 +0000

    add number 4

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a whole new doggo!
+ * @name: name of doggo
+ * @age: age of doggo
+ * @owner: owner of doggo
+ * Return: returns doggo's information
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
+
+	(*doggo).name = name;
+	(*doggo).age = age;
+	(*doggo).owner = owner;
+	return (doggo);
+}
