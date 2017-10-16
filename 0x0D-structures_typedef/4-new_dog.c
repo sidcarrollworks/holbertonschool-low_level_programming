d7ea631a6ef37bb47366fb4b3ef3c44223be8569
squidcarroll <squidcarroll@gmail.com>
2017-10-16 21:49:31 +0000

    add number 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *doggo;
 
 	doggo = malloc(sizeof(dog_t));
-	if (doggo == NULL)
+	if (dog_t == NULL)
 		return (NULL);
 
 	(*doggo).name = name;
