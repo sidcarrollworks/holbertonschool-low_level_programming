d56f434144aaff3a9e250e7ace540e3dc4e3dd12
squidcarroll <squidcarroll@gmail.com>
2017-10-16 23:29:07 +0000

    number 4 fix

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -17,8 +17,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	(*new_dog).name = name;
-	(*new_dog).age = age;
-	(*new_dog).owner = owner;
+	if (new_dog != NULL)
+	{
+		(*new_dog).name = *name;
+		(*new_dog).age = age;
+		(*new_dog).owner = *owner;
+	}
 	return (new_dog);
 }
