f8d8c120af1b9bd157b2d2c74ebf100174af5032
squidcarroll <squidcarroll@gmail.com>
2017-10-16 23:44:40 +0000

    number 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int _strlen(char *s);
+char *_strcpy(char *src);
+
 /**
  * new_dog - creates a whole new doggo!
  * @name: name of doggo
@@ -12,16 +15,51 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *nameCopy;
+	char *ownerCopy;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
+	nameCopy = _strcpy(name);
+	ownerCopy = _strcpy(owner);
 
-	if (dog_t != NULL)
-	{
-		(*new_dog).name = name;
-		(*new_dog).age = age;
-		(*new_dog).owner = owner;
-	}
+	(*new_dog).name = nameCopy;
+	(*new_dog).age = age;
+	(*new_dog).owner = ownerCopy;
 	return (new_dog);
+}
+/**
+ * _strlen - prints the length of a string
+ * Description: cycles through the array of s until its false
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0 ; s[len] ; len++);
+
+	return (len);
+}
+/**
+ * _strcpy - copys array from one pointer to another
+ * @dest: pointer to destination file
+ * @src: pointer to source file
+ * Return: the array
+ */
+char *_strcpy(char *src)
+{
+	int i;
+	int len;
+	char *dest;
+
+	len = _strlen(src);
+	dest = malloc(sizeof(char) * len + 1);
+	if (dest == NULL)
+		return (NULL);
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
