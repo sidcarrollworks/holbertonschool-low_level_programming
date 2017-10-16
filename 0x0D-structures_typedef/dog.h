479240de6c01328d83fc4cdefd5b16bc4aaaf393
squidcarroll <squidcarroll@gmail.com>
2017-10-16 21:32:19 +0000

    number 3

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -14,9 +14,10 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} my_dog;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#define dog_t struct dog
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
