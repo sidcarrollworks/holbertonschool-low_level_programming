364ec5dec27dd6115ddb2d974a04fbad49ef6a4b
squidcarroll <squidcarroll@gmail.com>
2017-10-16 21:22:06 +0000

    number 2

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -17,8 +17,8 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", (*d).age);
 
 		if ((*d).owner != NULL)
-			printf("Owner: %s", (*d).owner);
+			printf("Owner: %s\n", (*d).owner);
 		else
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 	}
 }
