3621e4cd8f367710ba8a93af234a52fefa8fd019
squidcarroll <squidcarroll@gmail.com>
2017-10-16 20:48:46 +0000

    number 1

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initializes the do structure
+ * @dog d - the dog structure
+ * @name - the name of the dog
+ * @age - 
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
