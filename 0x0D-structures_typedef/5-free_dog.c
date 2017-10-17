e8f942280095618a3b83b0af6db5a8340e8b34ae
squidcarroll <squidcarroll@gmail.com>
2017-10-17 00:01:36 +0000

    number 5

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ * free_dog - frees the dogs
+ * @d: the structure
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return (NULL);
+	free((*d).name);
+	free((*d).owner);
 	free(d);
 }
