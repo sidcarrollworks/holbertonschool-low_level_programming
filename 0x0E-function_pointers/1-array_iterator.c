459e12979cf5a35eb0ec1def37bc6a018a448a98
squidcarroll <squidcarroll@gmail.com>
2017-10-18 08:00:55 +0000

    number 1

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function on every index of array
+ * @array: the array we are indexing
+ * @size: the size of the array
+ * @action: the call to the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
