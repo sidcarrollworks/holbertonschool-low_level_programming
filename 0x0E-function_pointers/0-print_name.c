57c440c9c71dccb8ae9023a9b72d2d22ba6ce9d1
squidcarroll <squidcarroll@gmail.com>
2017-10-18 07:51:16 +0000

    number 0

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - calls the function and prints the name
+ * @name: the input string
+ * @f: the correct function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
