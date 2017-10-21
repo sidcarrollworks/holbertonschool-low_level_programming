c333f72df50ee8552e30df06ed73f048f5760a1c
squidcarroll <squidcarroll@gmail.com>
2017-10-21 22:08:07 +0000

    last check for 3

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -12,7 +12,7 @@ void print_all(const char * const format, ...)
 	va_list list;
 
 	va_start(list, format);
-	while (format[i])
+	while (format && format[i])
 	{
 		switch (format[i++])
 		{
@@ -37,6 +37,6 @@ void print_all(const char * const format, ...)
 		if (format[i])
 			printf(", ");
 	}
-	va_end(list);
 	putchar ('\n');
+	va_end(list);
 }
