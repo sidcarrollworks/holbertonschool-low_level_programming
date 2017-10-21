6c7519fe1254720cab822b4ddd63530a06f2b6a8
squidcarroll <squidcarroll@gmail.com>
2017-10-21 00:46:22 +0000

    number 3

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - prints all the things
+ * @format: contains the format modifiers
+ */
+void print_all(const char * const format, ...)
+{
+	char *holder;
+	int i = 0;
+	va_list list;
+	int c = 0;
+
+	va_start(list, format);
+	while (format[i] != '\0' && format != NULL)
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(list, int));
+				c = 0;
+				break;
+			case 'i':
+				printf("%d", va_arg(list, int));
+				c = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				c = 0;
+				break;
+			case 's':
+				holder = va_arg(list, char*);
+				if (holder != NULL)
+					printf("%s", holder);
+				else
+					printf("(nil)");
+				c = 0;
+				break;
+			default:
+				c = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && c == 0)
+			printf(", ");
+		i++;
+	}
+	va_end(list);
+	putchar ('\n');
+}
