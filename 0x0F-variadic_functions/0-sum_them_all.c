68e76012026b0e9187b7a78cbf725003289489c3
squidcarroll <squidcarroll@gmail.com>
2017-10-21 22:21:23 +0000

    number 0

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list list;
 
+	va_start(list, n);
+
 	if (n == 0)
 		return (0);
-
-	va_start(list, n);
 
 	for (i = 0 ; i < n ; i++)
 	{
