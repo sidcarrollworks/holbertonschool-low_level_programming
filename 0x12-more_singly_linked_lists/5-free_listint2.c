71869b59ebf7488cdf1c5383990ed3a31111dfcb
squidcarroll <squidcarroll@gmail.com>
2017-11-01 18:22:05 +0000

    fix files

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,7 @@ void free_listint2(listint_t **head)
 	listint_t *tmp;
 	listint_t *tmp2;
 
-<<<<<<< HEAD
-	if (head == NULL || *head == NULL)
-=======
 	if (!head || *head == NULL)
->>>>>>> 5b9e537f980fb2abc5ef80f6049519736ed9242e
 		return;
 
 	tmp = *head;
