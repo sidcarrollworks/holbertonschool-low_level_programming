0d35b7983eb9ff634568325038f60bdc822c5423
squidcarroll <squidcarroll@gmail.com>
2017-11-01 18:13:32 +0000

    free

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *traverse = *head;
 	unsigned int pos;
 
-	if (!head)
+	if (!(*head))
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
@@ -43,5 +43,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			pos++;
 		}
 	}
+	free(new_node);
 	return (NULL);
 }
