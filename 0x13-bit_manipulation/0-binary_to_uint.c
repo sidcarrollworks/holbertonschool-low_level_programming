a09aabdcf111d1c43bc5ccb8e3a223eae6e32939
squidcarroll <squidcarroll@gmail.com>
2017-11-02 21:25:56 +0000

    rewrote 0

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -6,9 +6,11 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num;
+	unsigned int num = 0;
 
-	for (num = 0 ; *b ; b++)
+	if (b == NULL)
+		return (0);
+	while (*b)
 	{
 		if (*b == '1')
 			num = (num << 1) | 1;
@@ -16,6 +18,7 @@ unsigned int binary_to_uint(const char *b)
 			num <<= 1;
 		else
 			return (0);
+		b++;
 	}
 	return (num);
 }
