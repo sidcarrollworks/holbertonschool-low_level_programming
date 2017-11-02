cbedf9a32f6d1364cd0e241d4933118f6d7ede8c
squidcarroll <squidcarroll@gmail.com>
2017-11-02 23:28:55 +0000

    number 0 final fix

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,14 +1,22 @@
 #include "holberton.h"
-
+void helper(unsigned long int n);
 /**
  * print_binary - print and int in binary
  * @n: number to convert
  */
 void print_binary(unsigned long int n)
 {
+	if (n == 0)
+		_putchar('0');
+	else
+		helper(n);
+}
+
+void helper(unsigned long int n)
+{
 	if (n)
 	{
-		print_binary(n >> 1);
+		helper(n >> 1);
 		if (n & 1)
 			_putchar('1');
 		else
