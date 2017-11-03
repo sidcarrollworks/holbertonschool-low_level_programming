7d78c8aa9d76a6ec2374212afd76db59230ef239
squidcarroll <squidcarroll@gmail.com>
2017-11-03 01:19:18 +0000

    fix stuff

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index <= sizeof(unsigned long int) * 4)
+	if (index <= sizeof(unsigned long int) * 8)
 		return ((n >> index) & 1);
 	return (-1);
 }
