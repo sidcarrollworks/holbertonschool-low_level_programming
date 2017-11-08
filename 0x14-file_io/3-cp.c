eb6760fa846d70c7e3c1d41fbbdd202b83d988d3
squidcarroll <squidcarroll@gmail.com>
2017-11-08 03:01:53 +0000

    number 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -9,7 +9,8 @@
 int main(int argc, char *argv[])
 {
 	char buf[1024];
-	int files[2];
+	int file1;
+	int file2;
 	int rERR = 1, wERR = 0;
 	int closeERR;
 
@@ -18,14 +19,14 @@ int main(int argc, char *argv[])
 		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	files[0] = open(argv[1], O_RDONLY);
-	if (files[0] == -1)
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
-	files[1] = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (files[1] == -1)
+	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file2 == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -33,19 +34,19 @@ int main(int argc, char *argv[])
 
 	while (rERR != 0)
 	{
-		rERR = read(files[0], buf, 1024);
+		rERR = read(file1, buf, 1024);
 		if (rERR == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		if (rERR != 0)
-			wERR = write(files[1], buf, rERR);
+			wERR = write(file2, buf, rERR);
 		if (wERR == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	closeERR = close(files[0]);
+	closeERR = close(file1);
 	if (closeERR == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", files[0]), exit(100);
-	closeERR = close(files[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1), exit(100);
+	closeERR = close(file2);
 	if (closeERR == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", files[1]), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2), exit(100);
 	return (0);
 }
