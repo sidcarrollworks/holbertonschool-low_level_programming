89b59307cdba29b5c432282121b60395569ab503
squidcarroll <squidcarroll@gmail.com>
2017-11-08 03:15:30 +0000

    number 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
