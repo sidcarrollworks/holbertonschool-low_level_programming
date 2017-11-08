b8c85420d15f0ea293f456c7d37cc9d4eb5f3a3c
squidcarroll <squidcarroll@gmail.com>
2017-11-08 05:52:55 +0000

    number 3 fix

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -16,10 +16,6 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	if (!argv[1])
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	if (!argv[2])
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
