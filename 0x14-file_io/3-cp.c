2352f2d07393566f36a8ba2502d7f733a573a2d7
squidcarroll <squidcarroll@gmail.com>
2017-11-08 05:32:21 +0000

    betty fix for 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -15,10 +15,7 @@ int main(int argc, char *argv[])
 	int closeERR;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
@@ -31,12 +28,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while (rERR != 0)
 	{
 		rERR = read(file1, buf, 1024);
 		if (rERR == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		if (rERR != 0)
 			wERR = write(file2, buf, rERR);
 		if (wERR == -1)
