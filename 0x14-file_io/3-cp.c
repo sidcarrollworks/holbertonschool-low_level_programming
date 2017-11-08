2895df12157ab7ac16d00a878b3c484f12bc972a
squidcarroll <squidcarroll@gmail.com>
2017-11-08 05:48:52 +0000

    number 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -16,6 +16,10 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	if (!argv[1])
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (!argv[2])
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
@@ -28,7 +32,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while (rERR != 0)
+	while (rERR > 0)
 	{
 		rERR = read(file1, buf, 1024);
 		if (rERR == -1)
