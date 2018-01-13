b93f30393dee75d617e232b5d47789af1e4c49f6
squidcarroll <squidcarroll@gmail.com>
2018-01-13 09:34:03 +0000

    update 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	new_table->array = malloc(sizeof(void *) * size);
 	if (!new_table->array)
 		return (NULL);
 	new_table->size = size;
