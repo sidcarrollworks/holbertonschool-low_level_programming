bca13ac4df1e3bb09a03f48155c05d306702e46b
squidcarroll <squidcarroll@gmail.com>
2018-01-13 10:40:55 +0000

    update 1

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -7,9 +7,10 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash = 5381;
+	unsigned long int hash;
 	int c;
 
+	hash = 5381;
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
