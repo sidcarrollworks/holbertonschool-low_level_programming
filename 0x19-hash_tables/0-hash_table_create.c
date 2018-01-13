8d5d0ff75f70a0be632e08192062f57338698392
squidcarroll <squidcarroll@gmail.com>
2018-01-13 09:32:39 +0000

    betty for 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,7 +1,9 @@
 #include "hash_tables.h"
 
 /*
- *
+ * hash_table_create - create hash table
+ * @size: size of array
+ * Return: return new hash table or NULL of failed
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
