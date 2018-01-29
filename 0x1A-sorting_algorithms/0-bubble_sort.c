9e8f40ce1a4df71de8f9db85e65fff4af9448bb8
squidcarroll <squidcarroll@gmail.com>
2018-01-29 20:13:33 +0000

    add checks for 0

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -10,6 +10,8 @@ void bubble_sort(int *array, size_t size)
 	unsigned int i, j;
 	int tmp;
 
+	if (!array || size == 0 || size == 1)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < (size - i - 1) ; j++)
