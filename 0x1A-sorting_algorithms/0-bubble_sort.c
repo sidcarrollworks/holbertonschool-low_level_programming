ec020ef6aa566824ed0e3135c64e27b962a0479b
squidcarroll <squidcarroll@gmail.com>
2018-01-29 20:17:01 +0000

    update 0

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -22,7 +22,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 
-				print_array(array, size);
+				print_array((const int *)array, size);
 			}
 		}
 	}
