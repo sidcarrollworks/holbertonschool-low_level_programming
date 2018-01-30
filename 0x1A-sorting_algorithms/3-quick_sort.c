fb8f1fbc7cd4b6b392e845276e80e0777bc9241d
squidcarroll <squidcarroll@gmail.com>
2018-01-30 21:41:54 +0000

    update 3

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -2,59 +2,80 @@
 void swap(int *a, int *b);
 int partition(int *array, int low, int high, int size);
 void sort(int *array, int low, int high, int size);
+
 /**
- *
- *
- *
+ * quick_sort - quick sort algo
+ * @array: array to be sorted
+ * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
-    int low;
+	int low;
 
-    low = 0;
-    if (size < 2 || array == NULL)
-        return;
-    size = size - 1;
-    sort(array, low, size, size + 1);
+	low = 0;
+	if (size < 2 || array == NULL)
+		return;
+	size = size - 1;
+	sort(array, low, size, size + 1);
 }
 
+/**
+ * sort - function that sorts array
+ * @array: array
+ * @low: low index
+ * @high: high part
+ * @size: size of array used to print
+ */
 void sort(int *array, int low, int high, int size)
 {
-    int part;
+	int part;
 
 	if (low < high)
-    {
-        part = partition(array, low, high, size);
-        sort(array, low, part - 1, size);
-        print_array(array, size);
-        sort(array, part + 1, high, size);
-    }
+	{
+		part = partition(array, low, high, size);
+		sort(array, low, part - 1, size);
+		print_array(array, size);
+		sort(array, part + 1, high, size);
+	}
 }
 
+/**
+ * partition - partitions array
+ * @array: array
+ * @low: low index
+ * @high: high index
+ * @size: size of array
+ * Return: partition
+ */
 int partition(int *array, int low, int high, int size)
 {
-    int pivot = array[high];
-    int i = (low - 1);
-    int j;
-    (void) size;
+	int pivot = array[high];
+	int i = (low - 1);
+	int j;
+	(void) size;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            swap(&array[i], &array[j]);
-        }
-    }
-    swap(&array[i + 1], &array[high]);
-    return (i + 1);
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	return (i + 1);
 }
 
+/**
+ * swap - functiont that swaps elements
+ * @a: first ellement
+ * @b: seceond element
+ */
 void swap(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
