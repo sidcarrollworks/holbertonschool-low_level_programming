bea8c30e6e2a9bce2cc3386ee11f2965d7b56d6b
squidcarroll <squidcarroll@gmail.com>
2018-01-30 13:16:57 +0000

    update all files

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -1,5 +1,7 @@
 #include "sort.h"
-
+void swap(int *a, int *b);
+int partition(int *array, int low, int high, int size);
+void sort(int *array, int low, int high, int size);
 /**
  *
  *
@@ -7,4 +9,48 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	if (
+    int low;
+
+    low = 0;
+    sort(array, low, size, size);
+}
+
+void sort(int *array, int low, int high, int size)
+{
+    int part;
+
+	if (low < high)
+    {
+        part = partition(array, low, high, size);
+        sort(array, low, part - 1, size);
+        sort(array, part + 1, high, size);
+    }
+}
+
+int partition(int *array, int low, int high, int size)
+{
+    int pivot = array[high];
+    int i = (low - 1);
+    int j;
+
+    for (j = low; j <= high - 1; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+            print_array(array, size);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+
+void swap(int *a, int *b)
+{
+    int tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
