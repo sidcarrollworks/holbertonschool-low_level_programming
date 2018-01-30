6403e941d302618df47c597ac5c9340c900ccccf
squidcarroll <squidcarroll@gmail.com>
2018-01-30 21:19:04 +0000

    update 2

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -16,12 +16,17 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
+
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min])
 				min = j;
-		tmp = array[min];
-		array[min] = array[i];
-		array[i] = tmp;
-		print_array(array, size);
+
+        if (min != i)
+        {
+		    tmp = array[min];
+		    array[min] = array[i];
+		    array[i] = tmp;
+		    print_array(array, size);
+        }
 	}
 }
