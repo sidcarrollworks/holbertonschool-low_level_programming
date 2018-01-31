38b0d8193b79317ead72aaf2e28c2a203f1956fd
squidcarroll <squidcarroll@gmail.com>
2018-01-31 23:27:51 +0000

    update files

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+void sort(listint_t **list, listint_t *node);
 
 /**
  * insertion_sort_list - insertion sorting method
@@ -6,4 +7,56 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *tmp = *list;
 
+	if (!list || !(*list) || !((*list)->next))
+		return;
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+		sort(list, tmp);
+	}
+}
+
+/**
+ * sort - this function holds the sorting logic
+ * @list: list to sort
+ * @node: current node
+ */
+void sort(listint_t **list, listint_t *node)
+{
+	listint_t *a;
+	listint_t *prv;
+	listint_t *nxt;
+
+	a = node->prev;
+
+	while (a && (a->n > node->n))
+	{
+		prv = a->prev;
+		nxt = node->next;
+		node->next = a;
+		a->prev = node;
+
+		if (prv)
+		{
+			node->prev = prv;
+			prv->next = node;
+		}
+		else
+		{
+			node->prev = NULL;
+			*list = node;
+		}
+
+		if (nxt)
+		{
+			a->next = nxt;
+			nxt->prev = a;
+		}
+		else
+			a->next = NULL;
+		print_list(*list);
+		a = node->prev;
+	}
+}
