7f20ac85ad4cf7be048a0d5d5c91cbc5692a0354
squidcarroll <squidcarroll@gmail.com>
2018-02-17 02:23:07 +0000

    update 5

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -2,18 +2,18 @@
 # find perimeter of island
 
 def island_perimeter(grid):
-	'''
-		island perimieter solution
-	'''
-	height = len(grid)
-	width = len(grid[0])
-	answer = 0
-	for x in range(height):
-		for y in range(width):
-			if grid[x][y] == 1:
-				answer += 4
-				if x > 0 and grid[x - 1][y]:
-					answer -= 2
-				if y > 0 and grid[x][y - 1]:
-					answer -= 2
-	return answer
+    '''
+        island perimieter solution
+    '''
+    height = len(grid)
+    width = len(grid[0])
+    answer = 0
+    for x in range(height):
+        for y in range(width):
+            if grid[x][y] == 1:
+                answer += 4
+                if x > 0 and grid[x - 1][y]:
+                    answer -= 2
+                if y > 0 and grid[x][y - 1]:
+                    answer -= 2
+    return answer
