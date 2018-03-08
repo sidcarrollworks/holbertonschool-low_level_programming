907d59aabf0a8e2e4b0596963eb132bef258f9c0
squidcarroll <squidcarroll@gmail.com>
2018-03-08 21:36:37 +0000

    update everything

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+int binary_tree_balance_helper(const binary_tree_t *tree);
 
 /**
  * binary_tree_balance - check if tree is balanced
@@ -7,19 +8,35 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int bal_l;
-	int bal_r;
+	int bal;
 
 	if (!tree)
 		return (0);
 
-	bal_l = binary_tree_balance(tree->left);
-	bal_r = binary_tree_balance(tree->right);
+	bal = binary_tree_balance_helper(tree->left);
+	bal -= binary_tree_balance_helper(tree->right);
 
-	if (tree->right)
-		bal_r++;
-	if (tree->left)
-		bal_l++;
+	return (bal);
+}
 
-	return (bal_l - bal_r);
+/**
+ * binary_tree_balance_helper - find height of a binary tree
+ * @tree: pointer to tree
+ * Return: The height of the tree or 0 if the tree is NULL
+ */
+int binary_tree_balance_helper(const binary_tree_t *tree)
+{
+	int left_height = 1, right_height = 1;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left != NULL)
+		left_height = 1 + binary_tree_balance_helper(tree->left);
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_balance_helper(tree->right);
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
