6d283fade326701c207d0c9c8f30601f38e7391f
squidcarroll <squidcarroll@gmail.com>
2018-03-08 20:59:42 +0000

    fix 15

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -12,12 +12,14 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		full = 1;
 	if (tree->left)
 		full = binary_tree_is_full(tree->left);
 	if (tree->right)
 		full = binary_tree_is_full(tree->right);
 
-	if ((!tree->right && tree->left) || (!tree->left && tree->right || (tree->left && tree->right))
+	if ((!tree->right && tree->left) || (!tree->left && tree->right))
 		full = 0;
 	return (full);
 }
