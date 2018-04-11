b8e6b35e0166f1d62b631e17308026b298855153
squidcarroll <squidcarroll@gmail.com>
2018-04-11 22:38:22 +0000

    push binary

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,53 +1,74 @@
 #include "search_algos.h"
 int binarySearch(int *array, int left, int right, int value, int size);
-void printArray(int *array, int size);
+int printArray(int *array, int left, int right);
 
 /**
- *
- *
+ * binary_search - main function
+ * @array: array
+ * @size: size of array
+ * @value: value to find
+ * Return: index where it was found
  */
 int binary_search(int *array, size_t size, int value)
 {
 	int val;
+
+	if (!array)
+		return (-1);
 
 	val = binarySearch(array, 0, size, value, size);
 	return (val);
 }
 
 /**
- *
- *
- *
+ * binarySearch - logic of the search
+ * @array: array
+ * @size: size of array
+ * @left: left
+ * @right: right
+ * @value: value to find
+ * Return: index value
  */
 int binarySearch(int *array, int left, int right, int value, int size)
 {
 	int mid;
 
-	if (right >= left)
+	size = printArray(array, left, right);
+	if (right > left)
 	{
-		mid = left + (right - left) / 2;
+		mid = left + (size) / 2;
 		if (array[mid] == value)
-			return mid;
+			return (mid);
+		else if (size == 1)
+			return (-1);
 
 		if (array[mid] > value)
-			return (binarySearch(array, left, mid - left, value, size));
-
-		return (binarySearch(array, mid + left, right, value, size));
-		printArray(array, size);
+			return (binarySearch(array, left, mid, value, size));
+		else
+			return (binarySearch(array, mid, right, value, size));
 	}
 	return (-1);
 }
 
-void printArray(int *array, int size)
+/**
+ * printArray - print array
+ * @array: array
+ * @left: left index
+ * @right: right index
+ * Return: size of array
+ */
+int printArray(int *array, int left, int right)
 {
-	int i;
+	int i = 0;
 
 	printf("Searching in array: ");
-	for (i = 0; i < size; i++)
+	for (; left < right ; left++)
 	{
-			printf("%i", array[i]);
-			if (i < size - 1)
-				printf(", ");
+		printf("%i", array[left]);
+		if (left < right - 1)
+			printf(", ");
+		i++;
 	}
 	printf("\n");
+	return (i);
 }
